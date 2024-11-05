51d7807bc60b3b80caa77214976da6f785ff16ff
Santiago Bernardi <santiagobernardi13@gmail.com>
2024-11-05 00:16:18 -0300

    perdon atilio github esta delicadito

--- TP3/EntregaFinal/js/cart.js
+++ TP3/EntregaFinal/js/cart.js
@@ -1,34 +1,35 @@
 class Cart {
-  constructor() {
-    this.products = new Map();
-  }
-
-  addProduct(id, product) {
-    if (this.products.has(id)) {
-      console.log(`El producto con ID ${id} ya está en el carrito.`);
-      return;
+    constructor() {
+      this.products = new Map();
     }
-    this.products.set(id, product);
-    console.log(`Producto con ID ${id} agregado al carrito.`);
+  
+    addProduct(id, product) {
+      if (this.products.has(id)) {
+        console.log(`El producto con ID ${id} ya está en el carrito.`);
+        return;
+      }
+      this.products.set(id, product);
+      console.log(`Producto con ID ${id} agregado al carrito.`);
+    }
+  
+    removeProduct(id) {
+      this.products.delete(id);
+      console.log(`Producto con ID ${id} eliminado del carrito.`);
+    }
+  
+    getTotalProducts() {
+      return this.products.size;
+    }
+  
+    listProducts() {
+      return Array.from(this.products.values());
+    }
+  
+    getTotal() {
+      return Array.from(this.products.values()).reduce(
+        (total, product) => total + product.price,
+        0
+      );
+    }
   }
-
-  removeProduct(id) {
-    this.products.delete(id);
-    console.log(`Producto con ID ${id} eliminado del carrito.`);
-  }
-
-  getTotalProducts() {
-    return this.products.size;
-  }
-
-  listProducts() {
-    return Array.from(this.products.values());
-  }
-
-  getTotal() {
-    return Array.from(this.products.values()).reduce(
-      (total, product) => total + product.price,
-      0
-    );
-  }
-}
+  
