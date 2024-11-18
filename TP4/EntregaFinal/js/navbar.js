8a2e8548ff41bb7b1fa4d77337249c7a214d2e1f
unicen-azuaga-nicolas <nazuaga@alumnos.exa.unicen.edu.ar>
2024-11-17 21:47:10 -0300

    fix: navbar animación

--- TP4/EntregaFinal/js/navbar.js
+++ TP4/EntregaFinal/js/navbar.js
@@ -1,8 +1,16 @@
 window.addEventListener("scroll", () => {
   const logo = document.getElementById("logo");
-  if (window.scrollY > 50) {
+  const navbar = document.getElementById("navbar");
+  console.log(window.scrollY);
+  if (window.scrollY > 100) {
     logo.classList.add("shrink");
   } else {
     logo.classList.remove("shrink");
+  }
+
+  if (window.scrollY > 959) {
+    navbar.classList.add("solid");
+  } else {
+    navbar.classList.remove("solid");
   }
 });
