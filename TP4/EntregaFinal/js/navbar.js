30e3f7e067fb51f71b49f714235a930a5905085a
unicen-azuaga-nicolas <nazuaga@alumnos.exa.unicen.edu.ar>
2024-11-20 19:24:20 -0300

    feat: menu hamburguesa y fix imagen

--- TP4/EntregaFinal/js/navbar.js
+++ TP4/EntregaFinal/js/navbar.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", () => {
   const logo = document.getElementById("logo");
   const navbar = document.getElementById("navbar");
-  console.log(window.scrollY);
+
   if (window.scrollY > 50) {
     logo.classList.add("shrink");
   } else {
@@ -12,5 +12,24 @@ window.addEventListener("scroll", () => {
     navbar.classList.add("solid");
   } else {
     navbar.classList.remove("solid");
+  }
+});
+
+const menuButton = document.getElementById("menu-toggle");
+let isOpenMenu = false;
+menuButton.addEventListener("click", () => {
+  isOpenMenu = !isOpenMenu;
+
+  const bars = document.getElementsByClassName("bar");
+
+  if (isOpenMenu) {
+    bars.item(0).classList.add("bar-rotate-left");
+    bars.item(1).classList.add("bar-display-none");
+    bars.item(2).classList.add("bar-rotate-rigth");
+  } else {
+    bars.item(0).classList.remove("bar-rotate-left");
+    bars.item(1).classList.remove("bar-display-none");
+    bars.item(2).classList.remove("bar-rotate-rigth");
+
   }
 });
